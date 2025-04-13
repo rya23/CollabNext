fdb2c46f4e09d06e48215d525ef3f29dd481e7bf
Nahsik16 <118239409+Nahsik16@users.noreply.github.com>
2025-04-13 10:22:33 +0530

    saved comment section

--- website/components/ui/editor/Toolbars.tsx
+++ website/components/ui/editor/Toolbars.tsx
@@ -50,7 +50,7 @@ export function SelectionToolbar({ editor }: Props) {
             <Toolbar.Separator className="w-[1px] h-7 bg-border mx-1" />
             <Toolbar.SectionInline className="flex items-center gap-1" />
             <Toolbar.Separator className="w-[1px] h-7 bg-border mx-1" />
-            <Toolbar.SectionCollaboration className="flex items-center gap-1" />
+           <div className="flex items-center gap-2 p-2"> <Toolbar.SectionCollaboration  /></div>
         </FloatingToolbar>
     );
 }
