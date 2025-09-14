7992fc2c2d62fd3456cf583bf975c8b20475aee4
Aarya <111475919+rya23@users.noreply.github.com>
2025-09-14 17:58:22 +0530

    Add badge prop to HeroGeometric component

--- website/app/page.tsx
+++ website/app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
         <div className="flex flex-col min-h-screen  ">
             <Navbar />
             <div className="container flex flex-1 flex-col mx-auto  ">
-                <HeroGeometric title1="CollabNext" title2="A Seam-less collaborative Text Editor" />
+                <HeroGeometric badge="NUCLITRON" title1="CollabNext" title2="A Seam-less collaborative Text Editor" />
                 <AboutUsSection />
             </div>
             <Footer />
