cd638fb0886f1edfe6604024eec9c1c3f08b93eb
Mustafa Sarangpurwala <84672838+HIDZI123@users.noreply.github.com>
2025-09-14 17:31:09 +0530

    Update HeroGeometric title for clarity

--- website/app/page.tsx
+++ website/app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
         <div className="flex flex-col min-h-screen  ">
             <Navbar />
             <div className="container flex flex-1 flex-col mx-auto  ">
-                <HeroGeometric badge="NUCLITRON" title1="CollabNext" title2="A project for KJ Hackathon" />
+                <HeroGeometric badge="NUCLITRON" title1="CollabNext" title2="A Seam-less collaborative Text Editor" />
                 <AboutUsSection />
             </div>
             <Footer />
